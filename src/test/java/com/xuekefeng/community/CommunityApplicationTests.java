2c3f3e478ad49f2141d11a10f2642515b9295e52
xkf0310 <50689303+xkf0310@users.noreply.github.com>
2020-01-22 20:25:13 +0800

    init repo

---
+++ src/test/java/com/xuekefeng/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuekefeng.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
